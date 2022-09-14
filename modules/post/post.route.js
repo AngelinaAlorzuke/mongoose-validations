aeaf6506d50f5d6e6d4e656fc66bbcaf39d8e05a
AngelinaAlorzuke <angelinaalorzuke333@gmail.com>
2022-09-14 08:04:49 +0000

    mongoose-validations-2

--- modules/post/post.route.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router
-const {createPost, getAllPosts} = require("./post.controller")
-
-
-const postsRouter = router()
-
-postsRouter.route("/").get(getAllPosts).post(createPost);
-
- module.exports = postsRouter;
